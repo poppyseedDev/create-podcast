57c7482b35f860c7c701c81d3b2bd7f4d5cb7ba2
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-06-18 13:34:01 +0200

    adding split text into chunks

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import os
 from text_to_speech import TextToSpeechAPI
 from constants import MAX_CHUNK_SIZE
+from split_text_into_chunks import TextSplitter
+
+def split_into_chunks(text, display=False):
+    splitter = TextSplitter(MAX_CHUNK_SIZE)
+    chunks = splitter.split_text(text)
+    
+    print("Splitting text into ", len(chunks), " chunks")
+
+    # Print the chunks and their lengths
+    if display:
+        for i, chunk in enumerate(chunks):
+            print(f"Chunk {i+1} (Length: {len(chunk)}):\n{chunk}\n{'-'*50}\n")
+
+    return chunks
 
 def go_through_chunks(api, chunks):
     for i, chunk in enumerate(chunks):
@@ -13,12 +27,10 @@ def main():
         text = file.read()
 
     api = TextToSpeechAPI("https://api.elevenlabs.io")  # Replace with the base URL of the API
-
-    # Split the text into chunks of max_chunk_size characters
-    chunks = [text[i:i + MAX_CHUNK_SIZE] for i in range(0, len(text), MAX_CHUNK_SIZE)]
     
-    print("Splitting text into ", len(chunks), " chunks")
-    go_through_chunks(api, chunks)
+    split_into_chunks(text, display=True)
+
+    #go_through_chunks(api, chunks)
 
 if __name__ == "__main__":
     main()
--- split_text_into_chunks.py
+++ split_text_into_chunks.py
@@ -1,3 +1,5 @@
+import nltk
+
 class TextSplitter:
     """
     A class used to split text into chunks.
@@ -16,46 +18,31 @@ class TextSplitter:
         trying to split on paragraph boundaries where possible.
     """
 
-    def __init__(self, max_chunk_size=500):
-        """
-        Constructs all the necessary attributes for the TextSplitter object.
-
-        Parameters
-        ----------
-        max_chunk_size : int
-            the maximum size for each chunk of text (default is 500)
-        """
+    def __init__(self, max_chunk_size=5000):
         self.max_chunk_size = max_chunk_size
 
     def split_text(self, text):
-        """
-        Splits the text into chunks of maximum size max_chunk_size, 
-        trying to split on paragraph boundaries where possible.
-
-        Parameters
-        ----------
-        text : str
-            long string to be split into chunks
-
-        Returns
-        -------
-        list
-            a list of strings, each string is a chunk of the original text
-        """
         chunks = []
         current_chunk = ""
-        for paragraph in text.split("\n"):
-            if len(current_chunk) + len(paragraph) < self.max_chunk_size:
-                current_chunk += paragraph + "\n"
+        
+        # Check if there are paragraph breaks
+        if '\n\n' in text:
+            units = text.split('\n\n')
+        elif '\n' in text:
+            units = text.split('\n')
+            
+        for unit in units:
+            if len(current_chunk) + len(unit) < self.max_chunk_size:
+                current_chunk += unit + "\n\n"
             else:
-                # If the paragraph is huge and more than max_chunk_size
-                while len(paragraph) >= self.max_chunk_size:
-                    current_chunk += paragraph[:self.max_chunk_size]
-                    paragraph = paragraph[self.max_chunk_size:]
+                # If the unit is huge and more than max_chunk_size
+                while len(unit) >= self.max_chunk_size:
+                    current_chunk += unit[:self.max_chunk_size]
+                    unit = unit[self.max_chunk_size:]
                     chunks.append(current_chunk)
                     current_chunk = ""
 
-                current_chunk += paragraph + "\n"
+                current_chunk += unit + "\n"
                 chunks.append(current_chunk)
                 current_chunk = ""
 
