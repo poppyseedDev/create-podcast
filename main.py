175be36f46bdd7cd7d001bbef11f22634209d329
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-06-18 11:53:57 +0200

    adding text to speech in different file to main

--- main.py
+++ main.py
@@ -1,52 +1,20 @@
-import requests
-import json
-from dotenv import dotenv_values
+import os
+from eleven_labs import TextToSpeechAPI
+# Assuming the TextToSpeechAPI class is defined as shown in the previous responses
 
+def main():
+    api = TextToSpeechAPI("https://api.elevenlabs.io")  # Replace with the base URL of the API
 
-class TextToSpeechAPI:
-    def __init__(self, base_url):
-        self.base_url = base_url
-        self.voice_id = "21m00Tcm4TlvDq8ikWAM"  # Replace with the desired voice ID
-        self.api_key = dotenv_values(".env.local").get("ELEVEN_LABS_API_KEY")  # Replace with the actual key name
+    text = """Long string of text..."""  # Replace with your big string of text
 
-    def convert_text_to_speech(self, text):
-        url = f"{self.base_url}/v1/text-to-speech/{self.voice_id}?optimize_streaming_latency=0"  # Replace with the actual URL
+    max_chunk_size = 5000
 
-        # Request body
-        body = {
-            "text": text,
-            "model_id": "eleven_monolingual_v1",
-            "voice_settings": {
-                "stability": 0.2,
-                "similarity_boost": 0.2
-            }
-        }
+    # Split the text into chunks of max_chunk_size characters
+    chunks = [text[i:i + max_chunk_size] for i in range(0, len(text), max_chunk_size)]
 
-        # Convert the body to JSON format
-        json_body = json.dumps(body)
+    for i, chunk in enumerate(chunks):
+        save_path = f"audio_{i}.mp3"  # Each result will be saved in a separate file
+        api.convert_text_to_speech(chunk, save_path)
 
-        # Set the headers
-        headers = {
-            "accept": "audio/mpeg",
-            "Content-Type": "application/json",
-            "xi-api-key": self.api_key
-        }
-
-        # Send the POST request
-        response = requests.post(url, data=json_body, headers=headers)
-
-        # Check the response
-        if response.status_code == 200:
-            # Successful request
-            response_data = response.json()
-            return response_data
-        else:
-            # Request failed
-            print("Request failed with status code:", response.status_code)
-            return None
-
-# Usage example
-api = TextToSpeechAPI("https://api.elevenlabs.io")  # Replace with the base URL of the API
-response_data = api.convert_text_to_speech("Hello, world!")  # Replace with the desired text
-if response_data:
-    print(response_data)
+if __name__ == "__main__":
+    main()
